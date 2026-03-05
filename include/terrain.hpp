10ff3f47fd493da428b8908f097277275c25355a
Julio <juliocesarsouaz@gmail.com>
2026-03-05 19:22:25 -0300

    Some stuff

---
+++ include/terrain.hpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <cstdlib>
+
+#ifndef TERRAIN_HPP
+#define TERRAIN_HPP
+namespace terrain{
+    using sizeType = int; 
+    class terrain{
+        public:
+        sizeType X;
+        sizeType Y;
+        sizeType Z;
+        terrain(sizeType axis, sizeType yxis, sizeType zxis);
+        private:
+            std::vector<double> terrainMap;
+            void perlin(sizeType x, sizeType y, sizeType z)
+
+        
+    }
+
+
+}
+#endif
